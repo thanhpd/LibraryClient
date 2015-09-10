f5c20076eae76dd7554a024db7ca4b37104d31b1
thanhpd <thanhpd.mac@gmail.com>
2015-09-10 12:57:28 +0700

    Added models

---
+++ LibraryData/Model/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryData.Model
+{
+    public class Book : LibItem
+    {        
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public string Publisher { get; set; }        
+
+        public string ImageUrl { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public string Status { get; set; }
+
+        public string Language { get; set; }
+
+        public string Summary { get; set; }
+
+        public string LocationCode { get; set; }
+
+        public virtual List<Author> Authors { get; set; }
+
+        public virtual List<Genre> Genres { get; set; }
+    }
+}
---
+++ LibraryData/Model/BookGenre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryData.Model
+{
+    public class BookGenre : LibItem
+    {
+        public Guid BookGuid { get; set; }
+
+        public Guid GenreGuid { get; set; }
+    }
+}
