d51d34ebb30b58ce9081e8499c09822780f48d28
thanhpd <thanhcan94@gmail.com>
2015-09-23 15:52:17 +0700

    Update description

--- LibraryDesktop/Models/BookModel.cs
+++ LibraryDesktop/Models/BookModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -7,34 +8,53 @@ using System.Threading.Tasks;
 using LibraryData.Models;
 using LibraryData.Services;
 using LibraryDesktop.Utils;
-using System.ComponentModel.DataAnno
 
 namespace LibraryDesktop.Models
 {
     public class BookModel
     {   
         [DisplayName("ID")]
-        [Description("The identify number of the book")]
+        [Description("The identify number of the book.")]
         public string id { get; set; }
 
+        [DisplayName("Image Url")]
+        [Description("The url path to the image.")]
         public string book_image { get; set; }
 
+        [DisplayName("Book Name")]
+        [Description("Current book's name.")]
         public string book_name { get; set; }
 
+        [DisplayName("Author")]
+        [Description("Author(s) of the book.")]
         public string book_author { get; set; }
 
+        [DisplayName("Publisher")]
+        [Description("Publisher of the book.")]
         public string book_publisher { get; set; }
 
+        [DisplayName("Date Created")]
+        [Description("Imported time of the book.")]
         public DateTime created_at { get; set; }
 
+        [DisplayName("Last Update")]
+        [Description("The most recent time that this book had been edited.")]
         public DateTime updated_at { get; set; }
 
+        [DisplayName("Year")]
+        [Description("Publish year of the book.")]
         public string book_year { get; set; }
 
-        public string status { get; set; }
+        [DisplayName("Is Active")]
+        [Description("Status of the book")]
+        public bool status { get; set; }
 
-        public string book_description { get; set; } 
+        [DisplayName("Description")]
+        [Description("The description of the book.")]
+        public string book_description { get; set; }
 
+        [DisplayName("Book Image")]
+        [Description("The cover image of the book.")]
         public Image BookImage { get; set; }
 
         public BookModel(Book book)
@@ -48,7 +68,7 @@ namespace LibraryDesktop.Models
             book_description = book.book_description;
             created_at = book.created_at;
             updated_at = book.updated_at;
-            status = book.status;
+            status = book.status == "1";
 
             if (!String.IsNullOrWhiteSpace(book_image))
             {
